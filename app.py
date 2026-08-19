6a345bd1b3d3ba09d8d081e2c431fe951cc24ce0
Vishakha Chavan <42957071+vishch24@users.noreply.github.com>
2026-08-20 00:00:45 +0530

    Set up Flask app with routes for home and health

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# Python Scripts
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
